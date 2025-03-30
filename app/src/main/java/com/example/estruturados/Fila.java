ccd178e3b9753186f2977ea28c52a9ef5625a034
Giovanni Strasser <giostrasser@gmail.com>
2025-03-30 15:29:38 -0300

    Implementa estrutura basica fila

---
+++ app/src/main/java/com/example/estruturados/Fila.java
@@ -0,0 +1,23 @@
+package com.example.estruturados;
+
+public class Fila {
+    private int primeiro;
+    private int ultimo;
+    private int dados[];
+    
+    public boolean cheia(){
+    }
+    
+    public boolean vazia(){
+    }
+    
+    public void insere(int valor){
+        
+    }
+    
+    public int remove(){
+    }
+    
+    public void imprime(){
+    }
+}
