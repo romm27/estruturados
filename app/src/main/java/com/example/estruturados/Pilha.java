1164629c296d1cd92d42d9fec2dde5317e32a46c
Giovanni Strasser <giostrasser@gmail.com>
2025-03-30 11:30:34 -0300

    Update Pilha.java

--- app/src/main/java/com/example/estruturados/Pilha.java
+++ app/src/main/java/com/example/estruturados/Pilha.java
@@ -11,4 +11,27 @@ public class Pilha {
     public boolean cheia(){
         return topo == dados.length -1;
     }
+    
+    public boolean empilhar(int valor){
+        if(cheia()){
+            System.out.println("Tamanho da fila insuficiente!");
+            return false; //<---- lidar com caso de tamanho insuficiente externamente
+        }
+        else{
+            dados[topo] = valor;
+            topo += 1;
+            return true;
+        }
+    }
+    
+    public int desempilhar(){
+        if(vazia()){
+            System.out.println("A pilha está vazia!");
+            return 0;
+        }
+        else{
+            topo -= 1;
+            return dados[topo];
+        }
+    }
 }
